06a7e9fc6c45db1a7b4b76c54cbf7fb4c6492945
. <abdufattarovjavlon@gmail.com>
2024-11-04 10:23:04 +0300

    fist task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String letter = scanner.nextLine();
+            addLetter(letter);
+            if(Objects.equals(letter, "q")){
+                break;
+            }
+        }
+    }
+    public static void addLetter(String Letter){
+        if(Letter.equals("p")){
+            Letter=Letter+"a";
+        }
+        else if(Letter.equals("l")){
+            Letter=Letter+"z";
+        }
+        else if (Letter.equals("m")) {
+            Letter=Letter+"q";
+        }
+        System.out.println(Letter);
     }
 }
